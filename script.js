ae3e162368590e8987b8e3d716457ead4cffe925
harperwallace <harperowwallace@gmail.com>
2022-11-01 08:47:18 +0100

    updated education

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+console.log('1')
+console.log(coll.length)
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        console.log('2')
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
+}
+
 const copyButtonLabel = "copy";
 
 // You can use a class selector instead if available. 
